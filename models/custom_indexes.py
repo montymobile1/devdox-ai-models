f976498bd2626db095d3fd80e426dacb3aa86c8f
Mohamad Ali Jaafar <mohamadali.jaafar@montyholding.com>
2025-07-30 13:30:25 +0300

    feature: Added the table responsible for tracking and claiming Q jobs
    Summary:
    - Added `QueueProcessingRegistry` model
    - created `models\custom_indexes.py` which houses all the partial indexes that we cant create via Tortoise ORM, and thus requires a raw sql creation.

---
+++ models/custom_indexes.py
@@ -0,0 +1,16 @@
+"""
+This module includes all the Partial indexes that need to be created, but since tortoise ORM does
+not support Partial creation, we have to put them here and use them in a central location
+"""
+import models
+
+QUEUE_PROCESSING_REGISTRY_ONE_CLAIM_UNIQUE_INDEX = f"""
+CREATE UNIQUE INDEX IF NOT EXISTS {models.queue_processing_registry_one_claim_unique}
+ON {models.QueueProcessingRegistry.Meta.table} (message_id)
+WHERE status IN ('pending', 'in_progress');
+"""
+
+CUSTOM_INDEXES = {
+    models.QueueProcessingRegistry.Meta.table: [QUEUE_PROCESSING_REGISTRY_ONE_CLAIM_UNIQUE_INDEX]
+}
+
