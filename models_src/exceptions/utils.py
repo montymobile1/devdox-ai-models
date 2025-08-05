7c0975a891f33b4238a22d0e51ad079588caf2e7
Mohamad Ali Jaafar <mohamadali.jaafar@montyholding.com>
2025-08-05 14:09:17 +0300

    feat: Introduced custom exception for project

---
+++ models_src/exceptions/utils.py
@@ -0,0 +1,47 @@
+import logging
+from enum import Enum
+
+from models_src.exceptions.base_exceptions import DevDoxModelsException
+from models_src.exceptions.exception_constants import (
+	LABEL_ALREADY_EXISTS_MESSAGE,
+	LABEL_ALREADY_EXISTS_TITLE,
+	MISSING_LABEL_ID_TITLE,
+	MISSING_LABEL_LOG_MESSAGE,
+	MISSING_USER_ID_LOG_MESSAGE,
+	MISSING_USER_ID_TITLE,
+	REPOSITORY_DOESNT_EXIST_MESSAGE, REPOSITORY_DOESNT_EXIST_TITLE,
+)
+
+
+def internal_error(log_message: str, error_type: str, log_level = logging.FATAL, **kwargs):
+
+    return DevDoxModelsException(
+        user_message=log_message,
+        log_message=log_message,
+        error_type=error_type,
+        log_level=log_level,
+        **kwargs
+    )
+
+class GitLabelErrors(Enum):
+    MISSING_USER_ID = {
+        "error_type": MISSING_USER_ID_TITLE,
+        "log_message": MISSING_USER_ID_LOG_MESSAGE,
+    }
+
+    MISSING_LABEL = {
+        "error_type": MISSING_LABEL_ID_TITLE,
+        "log_message": MISSING_LABEL_LOG_MESSAGE,
+    }
+
+    GIT_LABEL_ALREADY_EXISTS = {
+        "error_type": LABEL_ALREADY_EXISTS_TITLE,
+        "log_message": LABEL_ALREADY_EXISTS_MESSAGE,
+        "log_level": logging.ERROR
+    }
+
+class RepoErrors(Enum):
+    REPOSITORY_DOESNT_EXIST = {
+        "error_type": REPOSITORY_DOESNT_EXIST_TITLE,
+        "log_message": REPOSITORY_DOESNT_EXIST_MESSAGE,
+    }
