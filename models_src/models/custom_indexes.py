b576199679eb4a5e9ad0ba1b04158bb06115548c
Mohamad Ali Jaafar <mohamadali.jaafar@montyholding.com>
2025-08-13 13:53:24 +0300

    refactor: performed some fixes

--- models_src/models/custom_indexes.py
+++ models_src/models/custom_indexes.py
@@ -2,17 +2,17 @@
 This module includes all the Partial indexes that need to be created, but since tortoise ORM does
 not support Partial creation, we have to put them here and use them in a central location
 """
-import models
+from models_src.models import queue_processing_registry_one_claim_unique, QueueProcessingRegistry
 
 QUEUE_PROCESSING_REGISTRY_ONE_CLAIM_UNIQUE_INDEX = f"""
-DROP INDEX IF EXISTS {models.queue_processing_registry_one_claim_unique};
+DROP INDEX IF EXISTS {queue_processing_registry_one_claim_unique};
 
-CREATE UNIQUE INDEX IF NOT EXISTS {models.queue_processing_registry_one_claim_unique}
-ON {models.QueueProcessingRegistry.Meta.table} (message_id)
+CREATE UNIQUE INDEX IF NOT EXISTS {queue_processing_registry_one_claim_unique}
+ON {QueueProcessingRegistry.Meta.table} (message_id)
 WHERE status IN ('pending', 'in_progress');
 """
 
 CUSTOM_INDEXES = {
-    models.QueueProcessingRegistry.Meta.table: [QUEUE_PROCESSING_REGISTRY_ONE_CLAIM_UNIQUE_INDEX]
+    QueueProcessingRegistry.Meta.table: [QUEUE_PROCESSING_REGISTRY_ONE_CLAIM_UNIQUE_INDEX]
 }
 
