879e3725ca95d22b014dc9472f24dfd470ca47c6
Hayat Bourgi <hayat.bourgi@montyholding.com>
2025-08-20 12:03:10 +0300

    Remove empty `__init__.py` files and adjust imports in `__init__.py`

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 __version__ = "0.1.0"
 
-from .models_src.models import *
-from .utils.database import init_tortoise, close_tortoise
+from models_src.models import *
+from utils.database import init_tortoise, close_tortoise
 
 __all__ = [
     "init_tortoise",
