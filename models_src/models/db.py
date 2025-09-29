5e634205bec681b5db37c06935dc6d77d599bffc
Mohamad Ali Jaafar <mohamadali.jaafar@montyholding.com>
2025-09-29 12:27:04 +0300

    fix: While tortoise ORM is able to understand vectors via the `tortoise-vector` package and the `Vector` type it defines, but when using raw sql commands and calling it via connection, it's unable to understand what a vector is, so we have to register it on the connection via pgvector.
    Changelog:
    
    - added `pgvector==0.4.1` library to handle vectors on asyncpg level.
    
    - Introduced a common vector-specific Context Manager `PgVectorConnection` that can be attached to connections when using raw SQL that handles vectors
    
    - Implemented `PgVectorConnection` into the code:
    ```
    async def similarity_search(
            self,
            embedding: List[float],
            user_id: str | uuid.UUID,
            repo_id: str | uuid.UUID,
            limit: int = 5,
        ) -> List[Dict[str, Any]]:
    ```

--- models_src/models/db.py
+++ models_src/models/db.py
@@ -1,5 +1,6 @@
-from tortoise import Tortoise
-
+import asyncpg
+from tortoise import connections, Tortoise
+from pgvector.asyncpg import register_vector
 
 async def init_db(db_url: str, models: list[str]):
     await Tortoise.init(db_url=db_url, modules={"models": models})
@@ -8,3 +9,22 @@ async def init_db(db_url: str, models: list[str]):
 
 async def close_db():
     await Tortoise.close_connections()
+
+# Async context manager to get a Tortoise connection and register pgvector on demand
+class PgVectorConnection:
+    def __init__(self, alias: str = "default"):
+        self.db = connections.get(alias)
+        self.raw = None  # type: ignore
+    
+    async def __aenter__(self) -> asyncpg.Connection:
+        # Acquire a raw asyncpg.Connection
+        self.raw = await self.db._pool.acquire()
+        
+        # tell asyncpg how to handle pgvector
+        await register_vector(self.raw)
+        
+        return self.raw
+    
+    async def __aexit__(self, exc_type, exc, tb):
+        await self.db._pool.release(self.raw)
+        self.raw = None
