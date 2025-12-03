6557c56d7328446dfc67446ce6e5f02b6dbfe20e
mohamadali.jaafar <mohamadali.jaafar@montymobile.com>
2025-12-03 13:52:19 +0200

    fixed an issue after rebase

--- models_src/db_inits/tortoise_init.py
+++ models_src/db_inits/tortoise_init.py
@@ -89,6 +89,17 @@ def get_tortoise_config(
         "timezone": "UTC",
     }
 
+from tortoise import Tortoise
+
+
+async def init_db(db_url: str, models: list[str]):
+    await Tortoise.init(db_url=db_url, modules={"models": models})
+    await Tortoise.generate_schemas()
+
+
+async def close_db():
+    await Tortoise.close_connections()
+
 # Async context manager to get a Tortoise connection and register pgvector on demand
 class PgVectorConnection:
     def __init__(self, alias: str = "default"):
